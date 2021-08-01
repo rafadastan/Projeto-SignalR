a13357bdc8aaeed5fbf505c5cd400aa1826c1912
Raphael Augusto Pereira de Assis <raphael.assis@totvs.com.br>
2021-08-01 08:07:40 -0300

    projeto signalr

---
+++ Projeto02.Presentation.Mvc/Hubs/ContasHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Projeto02.Presentation.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto02.Presentation.Mvc.Hubs
+{
+    public class ContasHub : Hub
+    {
+        public async Task SendMessage(ContaViewModel contaViewModel)
+        {
+            contaViewModel.DataHora = DateTime.Now; //gerando data..
+
+            //criando uma rotina para as páginas que irão 'ouvir'
+            //os eventos (mensagens / notificações) que chegam no hub
+            await Clients.All.SendAsync("ReceiveMessage", contaViewModel);
+        }
+    }
+}
---
+++ Projeto02.Presentation.Mvc/Models/ContaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto02.Presentation.Mvc.Models
+{
+    public class ContaViewModel
+    {
+        public string Nome { get; set; }
+        public decimal? Valor { get; set; }
+        public int? Tipo { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}
